0e0167eeffc091f1f4d026569af2a6c5edfbe015
Arav Agarwal <aravagarwal3073@gmail.com>
2020-06-22 14:42:07 -0400

    Remove R commands to see if C++ just works

--- rAbseil/src/rcpp_hello_world.cpp
+++ rAbseil/src/rcpp_hello_world.cpp
@@ -1,9 +1,15 @@
 
-#include <Rcpp.h>
+//#include <Rcpp.h>
+#include <iostream>
 #include "absl/time/clock.h"
-using namespace Rcpp;
+//using namespace Rcpp;
 
 // [[Rcpp::export]]
 double rcpp_hello_world() {
-    return GetCurrentTimeNanos();
+    return static_cast<double>(absl::lts_2020_02_25::GetCurrentTimeNanos());
+}
+
+int main() {
+    std::cout << rcpp_hello_world() << std::endl;
+    return 0;
 }
